ade8ac7ca7085fc1781d28bd1fdd04c0517282f2
Zhifeng Yao <zhifengyao731@gmail.com>
2026-07-27 15:50:57 -0400

    不记得改了啥

---
+++ Frontend/src/Data/IndexDB/index.ts
@@ -0,0 +1,13 @@
+export { closeIndexDb, getIndexDb } from "./database";
+export {
+  createIndexDbRepository,
+  type IndexDbRepository,
+} from "./repository";
+export {
+  INDEX_DB_STORE_NAMES,
+  type IndexDbError,
+  type IndexDbErrorCode,
+  type IndexDbRecord,
+  type IndexDbResult,
+  type IndexDbStoreName,
+} from "./types";
